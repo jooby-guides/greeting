494b9fbfa51500dfbe61d12bc4a43a1f1cea04b4
Edgar Espina <espina.edgar@gmail.com>
2017-01-29 21:04:41 -0300

    update integration tests to use rest-assured

--- src/test/java/greeting/AppTest.java
+++ src/test/java/greeting/AppTest.java
@@ -1,16 +1,25 @@
 package greeting;
 
+import static io.restassured.RestAssured.get;
+import static org.hamcrest.Matchers.equalTo;
+import org.jooby.test.JoobyRule;
 import org.junit.Test;
+import org.junit.ClassRule;
 
 /**
  * @author jooby generator
  */
-public class AppTest extends BaseTest {
+public class AppTest {
+
+  @ClassRule
+  public static JoobyRule app = new JoobyRule(new App());
 
   @Test
   public void greeting() throws Exception {
-    server.get("/greeting?name=Jooby")
-        .expect("{\"id\":1,\"name\":\"Hello Jooby!\"}")
-        .header("Content-Type", "application/json;charset=UTF-8");
+    get("/greeting?name=Jooby")
+        .then()
+        .assertThat()
+        .body(equalTo("{\"id\":1,\"name\":\"Hello Jooby!\"}"))
+        .contentType("application/json;charset=UTF-8");
   }
 }
